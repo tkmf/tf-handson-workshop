4a298e27a973282475e6479d7057a0ccb52179a3
00112357 <Takumi.Fujiwara@jp.yokogawa.com>
2020-10-15 16:43:04 +0900

    second commit

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = "~> 0.12" 
-  features {}
 }
 
 provider "azurerm" {
@@ -8,6 +7,7 @@ provider "azurerm" {
   tenant_id = var.tenant_id
   subscription_id = var.subscription_id
   client_secret = var.client_secret
+  features {}
 }
 
 resource "azurerm_virtual_machine" "main" {
